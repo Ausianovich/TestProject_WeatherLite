69e33c9ab12fb13e7cf6dd818d779cb0f6c56e8e
Ausianovich Kanstantsin <smilekostik@yandex.ru>
2021-02-27 16:29:05 +0300

    Initial Commit

---
+++ WeatherLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherLite
+//
+//  Created by Ausianovich Kanstantsin on 27.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
