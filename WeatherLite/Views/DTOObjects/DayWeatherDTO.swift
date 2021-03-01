56d3b75eb1729011a2098cbe4411fb31b6325d3b
Ausianovich Kanstantsin <smilekostik@yandex.ru>
2021-03-01 22:18:06 +0300

    1. Add DayWeatherDTO 2. Add HourlyWeatherDTO

---
+++ WeatherLite/Views/DTOObjects/DayWeatherDTO.swift
@@ -0,0 +1,20 @@
+//
+//  DayWeatherDTO.swift
+//  WeatherLite
+//
+//  Created by Ausianovich Kanstantsin on 01.03.2021.
+//
+
+import Foundation
+
+struct DayWeatherDTO {
+    var location: String?
+    var timeCode: String?
+    var temperature: String?
+    var pressure: String?
+    var humidity: String?
+    var windSpeed: String?
+    var windDerection: String?
+    var weatherCondition: String?
+    var rain: String?
+}
---
+++ WeatherLite/Views/DTOObjects/HourlyWeatherDTO.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyWeatherDTO.swift
+//  WeatherLite
+//
+//  Created by Ausianovich Kanstantsin on 01.03.2021.
+//
+
+import Foundation
+
+struct HourlyWeatherDTO {
+    var hourlyData: [DayWeatherDTO] = []
+}
