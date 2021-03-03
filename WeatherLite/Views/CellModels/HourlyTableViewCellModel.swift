962b7715401992b011b34198edd326e13ca08b7f
Ausianovich Kanstantsin <smilekostik@yandex.ru>
2021-03-03 23:03:13 +0300

    1. Create HourlyTableViewCellModel 2. Create HourlyTableViewSectionCellModel

---
+++ WeatherLite/Views/CellModels/HourlyTableViewCellModel.swift
@@ -0,0 +1,16 @@
+//
+//  HourlyTableViewCellModel.swift
+//  WeatherLite
+//
+//  Created by Ausianovich Kanstantsin on 02.03.2021.
+//
+
+import Foundation
+import UIKit
+
+struct HourlyTableViewCellModel {
+    let image: UIImage?
+    let timeCode: String
+    let weatherCondition: String
+    let temperature: String
+}
---
+++ WeatherLite/Views/CellModels/HourlyTableViewSectionCellModel.swift
@@ -0,0 +1,13 @@
+//
+//  HourlyTableViewSectionCellModel.swift
+//  WeatherLite
+//
+//  Created by Ausianovich Kanstantsin on 03.03.2021.
+//
+
+import Foundation
+
+struct HourlyTableViewSectionCellModel {
+    var title: String
+    var data: [HourlyTableViewCellModel]
+}
